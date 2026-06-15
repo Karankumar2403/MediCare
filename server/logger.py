8229e0a9f05365870a495d3a575ea4e1a3ee9015
karan <itskaranraj009@gmail.com>
2026-06-15 21:40:22 +0530

    first commit - ignore environment variables

---
+++ server/logger.py
@@ -0,0 +1,18 @@
+import logging
+def setup_logger(name="MediCare"):
+      logger=logging.getLogger(name)
+      logger.setLevel(logging.DEBUG)
+      ch=logging.StreamHandler()
+      ch.setLevel(logging.DEBUG)
+      formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+      ch.setFormatter(formatter)
+      if not logger.hasHandlers():
+            logger.addHandler(ch)
+
+      return logger
+
+logger=setup_logger()
+logger.info("RAG process started")
+logger.debug("Debugging information")
+logger.error("An error occurred")
+logger.critical("Critical issue encountered")
